5c6d87d174e3278b2410eb12a9ca34c36ffaeec1
Stefan Kuznetsov <neosloth@disroot.org>
2018-04-23 18:59:44 -0700

    Switched to plists for card storage

--- cl-scryfall.lisp
+++ cl-scryfall.lisp
@@ -5,9 +5,9 @@
 ;;; "cl-scryfall" goes here. Hacks and glory await!
 
 (defun search-card (query)
-  "Find a card and return it as an alist. Returns nil if no cards are found."
+  "Find a card and return it as a plist. Returns nil if no cards are found."
   (let* ((url (build-url :path "/cards/search?q=" :args query))
          (json (load-url-as-json url)))
     (cond
-      ((equalp (cdr (sassoc "object" json)) "error") nil)
-      (t (cadr (sassoc "data" json))))))
+      ((equalp (getf json :|object|) "error") nil)
+      (t (getf json :|data|)))))
--- util.lisp
+++ util.lisp
@@ -5,11 +5,8 @@
 (defun build-url (&key path args)
   (format nil "~a~a~a" *scryfall* path (drakma:url-encode args :utf-8)))
 
-(defun load-url-as-json (url &optional (type :alist))
+(defun load-url-as-json (url &optional)
   "Load a given url into an alist"
   (jonathan:parse
    (flexi-streams:octets-to-string
-    (drakma:http-request url :method :get :user-agent *user-agent*)) :as type))
-
-(defun sassoc (key list)
-  (assoc key list :test #'equalp))
+    (drakma:http-request url :method :get :user-agent *user-agent*))))
