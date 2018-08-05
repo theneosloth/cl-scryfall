03492e19f0df074e06c575fed3ec9003fc5fe475
Stefan Kuznetsov <neosloth@disroot.org>
2018-08-04 21:05:29 -0700

    Finished the implementation of card, set and rulings api calls

--- src/cards.lisp
+++ src/cards.lisp
@@ -2,11 +2,20 @@
 
 (in-package #:cl-scryfall)
 
-(defun cards-cards (&optional page)
+;; Utilty functions
+
+(defun create-card (list)
+  (apply #'make-card list))
+
+(defun create-cards (list)
+  (mapcard #'create-card list))
+
+;; API Calls
+(defun cards-cards (page)
   "Returns a List object that contains all cards in Scryfall’s database."
   (with-api-call "/cards"
     (epairlis '("page") (list page))
-    json))
+    (create-cards json)))
 
 (defun cards-search (q &optional &key unique order dir include_extras page)
   "Returns a List object containing Cards found using a fulltext search string.
@@ -14,48 +23,53 @@
   (with-api-call "/cards/search"
     (epairlis '("q" "unique" "order" "dir" "include_extras" "page")
               (list q unique order dir include_extras page))
-    json))
+    (create-cards json)))
 
 (defun cards-named-exact (name &optional set)
   "Card with that exact NAME is returned."
   (with-api-call "/cards/named"
     (epairlis '("exact" "set") (list name set))
-    json))
+    (apply #'make-card json)))
 
 (defun cards-named-fuzzy(name &optional set)
   "Execute fuzzy search on NAME."
   (with-api-call "/cards/named"
     (epairlis '("fuzzy" "set") (list name set))
-    json))
+    (apply #'make-card json)))
 
 (defun cards-autocomplete (q)
-  "Returns a Catalog object containing up to 20 full English card names that could be autocompletions of the given string parameter."
+  "Returns a list of strings containing up to 20 full English card names that could be autocompletions of the given string parameter."
   (with-api-call "/cards/autocomplete"
     (epairlis '("q") (list q))
-    json))
+    (getf json :data)))
 
 (defun cards-random ()
   "Returns a single random Card object."
   (with-api-call "/cards/random/" nil
-                 json))
+                 (apply #'make-card json)))
 
 (defun cards-multiverse (id)
   "Returns a single card with the given Multiverse ID. If the card has multiple multiverse IDs, this method can find either of them."
   (with-api-call (format nil "/cards/multiverse/~a" id) nil
-                 json))
+                 (apply #'make-card json)))
 
 (defun cards-mtgo (id)
   "Returns a single card with the given MTGO ID (also known as the Catalog ID).
  The ID can either be the card’s mtgo_id or its mtgo_foil_id."
   (with-api-call (format nil "/cards/mtgo/~a" id) nil
-                 json))
+                 (apply #'make-card json)))
+
+(defun cards-arena (id)
+  "Returns a single card with the given Magic: The Gathering Arena ID."
+  (with-api-call (format nil "/cards/arena/~a" id) nil
+                 (apply #'make-card json)))
 
 (defun cards-code-number (code number)
   "Returns a single card with the given set code and collector number."
   (with-api-call (format nil "/cards/~a/~a" code number) nil
-                 json))
+                 (apply #'make-card json)))
 
-(defun cards-id(id)
+(defun cards-id (id)
   "Returns a single card with the given Scryfall ID."
   (with-api-call (format nil "/cards/~a" id) nil
-                 json))
+                 (apply #'make-card json)))
